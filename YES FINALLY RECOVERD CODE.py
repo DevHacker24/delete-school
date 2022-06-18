7810bdc2e00afed50819cbfb1513fcd49f4a00fa
DevHacker24 <87660409+DevHacker24@users.noreply.github.com>
2022-06-18 15:32:20 +0530

    Add files via upload

---
+++ YES FINALLY RECOVERD CODE.py
@@ -0,0 +1,73 @@
+import sys
+import time
+sentence1 = "Intisialising deleting school misssion..."
+sentence2 = "Hacking database..."
+sentence3 = "Intisialising missile launch..."
+sentence10 = "Hacking Succsesful..."
+sentence4 = "Missile not launching, aborting mission..."
+sentence5 = "Removing hacking traces..."
+sentence6 = "Mission succsesfully failed."
+time.sleep(2)
+for char in sentence1:
+    time.sleep(0.08)
+    sys.stdout.write(char)
+    sys.stdout.flush()
+
+time.sleep(2.5)
+
+print("")
+
+
+for char in sentence2:
+    time.sleep(0.08)
+    sys.stdout.write(char)
+    sys.stdout.flush()
+
+
+
+time.sleep(2.5)
+
+print("")
+
+for char in sentence10:
+    time.sleep(0.08)
+    sys.stdout.write(char)
+    sys.stdout.flush()
+
+time.sleep(2.5)
+
+print("")
+
+for char in sentence3:
+    time.sleep(0.08)
+    sys.stdout.write(char)
+    sys.stdout.flush()
+
+time.sleep(2.5)
+print("")
+
+for char in sentence4:
+    time.sleep(0.08)
+    sys.stdout.write(char)
+    sys.stdout.flush()
+
+time.sleep(2.5)
+print("")
+
+
+
+for char in sentence5:
+    time.sleep(0.08)
+    sys.stdout.write(char)
+    sys.stdout.flush()
+
+
+time.sleep(2.5)
+print("")
+
+
+
+for char in sentence6:
+    time.sleep(0.08)
+    sys.stdout.write(char)
+    sys.stdout.flush()
